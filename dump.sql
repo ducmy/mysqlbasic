012d6217b8855bf478064f1afba44c36e8123d0f
Nguyễn Đức Mỹ <nguyenducmy393@gmail.com>
2022-06-26 12:06:35 +0700

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,11 +1,5 @@
-## Creata Table
+show tables;
 
-CREATE TABLE `photos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -15,7 +9,15 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 
-## dump_data
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+
+-- Insert Data
 
 INSERT INTO users
 (id, username, email)
@@ -31,11 +33,46 @@ VALUES(1, 'http://img1.jpg', 4),
 (3, 'http://img3.jpg', 1),
 (4, 'http://img4.jpg', 2);
 
+-- Check photo & user
+SELECT * FROM photos;
+SELECT * FROM  users;
+
+-- User has photo list
+
+-- Join
+SELECT users.id, users.username, JSON_ARRAYAGG(photos.url) as images from users, photos  
+WHERE users.id = photos.user_id
+group by users.id
+order by users.username;
+
+SELECT users.id, users.username, JSON_ARRAYAGG(photos.url) as images FROM users
+left join photos on users.id = photos.user_id
+group by users.id
+order by users.username;
 
 
+-- Photo belong user
+SELECT photos.id, photos.url, users.username FROM photos, users
+WHERE users.id = photos.user_id
+order by id;
+
+-- CRUD Photos
+-- Add new record
+INSERT INTO photos (url, user_id)
+values ('http://img5.jpg', 3);
+
+SELECT * FROM photos WHERE user_id = 3;
+
+update photos
+set url = 'http://img22.jpg'
+where id = :photo_id;
+
+DELETE from photos
+WHERE id = :photo_id;
 
 
+SELECT * FROM photos WHERE id = :photo_id;
 
 
-
-
+DROP table users;
+DROP table photos;
